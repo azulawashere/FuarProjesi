acf766e7d88505d9054c221942bc3477370200e4
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-11-08 21:16:48 +0300

    Configurations acıldı

---
+++ FuarProjesi/Models/Configurations/BaseConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using FuarProjesi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FuarProjesi.Models.Configurations
+{
+    public abstract class BaseConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            
+        }
+    }
+}
