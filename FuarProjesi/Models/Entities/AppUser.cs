59fad0d5e796db0daf1de121293472be508cd556
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2023-11-08 21:16:11 +0300

    AppUser ve AppUserProfile classları oluşturuldu.

---
+++ FuarProjesi/Models/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace FuarProjesi.Models.Entities
+{
+    public class AppUser : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        //Relational Properties
+        public virtual AppUserProfile Profile { get; set; }
+    }
+}
