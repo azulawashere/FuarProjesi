a29188c47688d655da521d05f4a4bfd0c4198dd8
Özenç Geçkin <baranx9@hotmail.com>
2023-11-08 21:20:06 +0300

    Mycontext eklend

--- FuarProjesi/Program.cs
+++ FuarProjesi/Program.cs
@@ -1,9 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using FuarProjesi.Models.ContextClasses;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddDbContextPool<MyContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")).UseLazyLoadingProxies());
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
