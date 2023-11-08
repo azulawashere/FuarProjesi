d9e38eb0a280026f14fff22c8df3822b780b5b84
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2023-11-08 21:11:13 +0300

    BaseEntity açıldı

---
+++ FuarProjesi/Models/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace FuarProjesi.Models.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+        }
+        public int ID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+    }
+}
