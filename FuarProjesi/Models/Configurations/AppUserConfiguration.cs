769e2435f2026ef5caaf5b59716930c0d4c91923
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-11-08 21:22:30 +0300

    s

---
+++ FuarProjesi/Models/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using FuarProjesi.Models.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FuarProjesi.Models.Configurations
+{
+    public class AppUserConfiguration:BaseConfiguration<AppUser>
+    {
+        public override void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            base.Configure(builder);
+            builder.HasOne(x => x.Profile).WithOne(x=>x.AppUser).HasForeignKey<AppUserProfile>(x=>x.ID) ;
+        }
+    }
+}
---
+++ FuarProjesi/Models/Configurations/ProfileConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using FuarProjesi.Models.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FuarProjesi.Models.Configurations
+{
+    public class ProfileConfiguration:BaseConfiguration<AppUserProfile>
+    {
+        public override void Configure(EntityTypeBuilder<AppUserProfile> builder)
+        {
+            base.Configure(builder);
+        }
+    }
+}
