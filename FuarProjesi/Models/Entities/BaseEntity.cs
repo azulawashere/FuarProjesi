c764c8756e2327c53efec933938eb3bec7379dfc
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2023-11-08 21:12:31 +0300

    .

--- FuarProjesi/Models/Entities/BaseEntity.cs
+++ FuarProjesi/Models/Entities/BaseEntity.cs
@@ -1,15 +1,19 @@
-﻿namespace FuarProjesi.Models.Entities
+﻿using FuarProjesi.Models.Enums;
+
+namespace FuarProjesi.Models.Entities
 {
     public class BaseEntity
     {
         public BaseEntity()
         {
             CreatedDate = DateTime.Now;
+            Status = DataStatus.Inserted;
         }
         public int ID { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
+        public DataStatus Status { get; set; }
 
     }
 }
