d2a8a322347cdcb6ee527fb4b9f8e1f6df547af8
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-11-12 19:09:14 +0300

    s

--- FuarProjesi/Controllers/RegisterController.cs
+++ FuarProjesi/Controllers/RegisterController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FuarProjesi.Models.Register.RequestModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FuarProjesi.Controllers
 {
     public class RegisterController : Controller
     {
         public IActionResult SignUp()
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(RegisterRequestModel register)
         {
 
 
