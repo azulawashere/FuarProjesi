cf617d24c1cb45c5e15fbfc0be479470617d9466
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2023-11-12 19:47:23 +0300

    x

--- FuarProjesi/Models/LogIn/RequestModels/LogInRequestModel.cs
+++ FuarProjesi/Models/LogIn/RequestModels/LogInRequestModel.cs
@@ -5,9 +5,13 @@ namespace FuarProjesi.Models.LogIn.RequestModels
     public class LogInRequestModel
     {
         [Required(ErrorMessage ="{0} is required!")]
+        [MinLength(5, ErrorMessage = "Min {1} karakter giriniz")]
+        [MaxLength(15, ErrorMessage = "Max {1} karakter giriniz")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "{0} is required!")]
+        [MinLength(5, ErrorMessage = "Min {1} karakter giriniz")]
+        [MaxLength(15, ErrorMessage = "Max {1} karakter giriniz")]
         public string Password { get; set; }
     }
 }
