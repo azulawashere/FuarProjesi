3aedf3e66c0150b9ad9d54dd7dedc95910dac083
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2023-11-12 19:26:41 +0300

    x

--- FuarProjesi/Models/LogIn/RequestModels/LogInRequestModel.cs
+++ FuarProjesi/Models/LogIn/RequestModels/LogInRequestModel.cs
@@ -1,8 +1,13 @@
-﻿namespace FuarProjesi.Models.LogIn.RequestModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuarProjesi.Models.LogIn.RequestModels
 {
     public class LogInRequestModel
     {
+        [Required(ErrorMessage ="{0} is required!")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!")]
         public string Password { get; set; }
     }
 }
