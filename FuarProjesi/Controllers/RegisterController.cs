3ed3b3604cfe9c5adcb06d6a57fa0b1b750ccbe8
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-11-12 19:26:26 +0300

    s

--- FuarProjesi/Controllers/RegisterController.cs
+++ FuarProjesi/Controllers/RegisterController.cs
@@ -1,10 +1,19 @@
-﻿using FuarProjesi.Models.Register.RequestModels;
+﻿using FuarProjesi.Models.ContextClasses;
+using FuarProjesi.Models.Entities;
+using FuarProjesi.Models.Register.PageVMs;
+using FuarProjesi.Models.Register.RequestModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FuarProjesi.Controllers
 {
     public class RegisterController : Controller
     {
+        MyContext _db;
+        public RegisterController(MyContext db)
+        {
+            _db = db;
+            
+        }
         public IActionResult SignUp()
         {
 
@@ -14,7 +23,39 @@ namespace FuarProjesi.Controllers
         [HttpPost]
         public IActionResult SignUp(RegisterRequestModel register)
         {
+            try
+            {
+                if (register.UserName!=string.Empty&&register.Password!=string.Empty)
+                {
+                    AppUserProfile profile = new()
+                    {
+                     BirthDate=register.BirthDate,
+                     FirstName=register.FirstName,
+                     LastName=register.LastName,
+                    };
+                    AppUser user = new()
+                    {
+                        UserName = register.UserName,
+                        Password = register.Password,
+                        Profile=profile,
+                        Admin=false
+                    };
+                    _db.Add(user);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index","Login");
 
+                }
+
+            }
+            catch 
+            {
+
+                RegisterUserSharedPageVM vm = new()
+                {
+                    Register = register
+                };
+                return View(vm);
+            }
 
             return View();
         }
