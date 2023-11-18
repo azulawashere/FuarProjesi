a418f24445c3edfb94d47f06f5901413bd20fb6e
Anıl Umut Erdoğan <anilumuterdogan@gmail.com>
2023-11-18 22:06:20 +0300

    x

---
+++ FuarProjesi/Controllers/PlaceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FuarProjesi.Controllers
+{
+    public class PlaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePlace()
+        {
+            return View();
+        }
+    }
+}
