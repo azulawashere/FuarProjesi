ee428853dca714709f96130b13a50e8d29198d47
Özenç Geçkin <baranx9@hotmail.com>
2023-11-18 19:29:03 +0300

    ad

---
+++ FuarProjesi/Migrations/20231118161057_18O.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FuarProjesi.Migrations
+{
+    /// <inheritdoc />
+    public partial class _18O : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
